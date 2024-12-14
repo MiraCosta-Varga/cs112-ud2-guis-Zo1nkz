0bfffdf5ee509d1fa35981ca3638bfd4f0d3dcfa
Zo1nkz <zachpcolinco@gmail.com>
2024-12-14 12:44:47 -0800

    Functionality Complete; add aesthetics

---
+++ src/main/java/cs112/ud2/controllers/StudentAddController.java
@@ -0,0 +1,65 @@
+package cs112.ud2.controllers;
+
+import cs112.ud2.MainApplication;
+import cs112.ud2.models.Student;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StudentAddController {
+
+    @FXML
+    private TextField studentNameInput;
+
+    @FXML
+    private TextField studentGradeLevelInput;
+
+    @FXML
+    private TextField studentRaceInput;
+
+    @FXML
+    private TextField studentMathGradesInput;
+
+    @FXML
+    private TextField studentEnglishGradesInput;
+
+    @FXML
+    private Button AddStudentButton2;
+
+
+    @FXML
+    protected void onAddStudentButton2Click(ActionEvent actionEvent) throws IOException {
+        //create the student object from the input data
+        Student stu = new Student(studentNameInput.getText(), studentRaceInput.getText(), Integer.parseInt(studentGradeLevelInput.getText()), studentMathGradesInput.getText(), studentEnglishGradesInput.getText());
+        System.out.println(stu);
+        SharedData.addStudent(stu);
+
+        //get the map view as a scene
+        //Parent studentListView = FXMLLoader.load(MainApplication.class.getResource("StudentList-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("StudentList-view.fxml"));
+        Parent studentListView = loader.load();
+        Scene studentListViewScene = new Scene(studentListView);
+
+        //set the student object to the next scene
+        StudentListController controller = loader.getController();
+        controller.setStudent();
+
+        //get stage
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        //set the scene (change it) for the stage (window)
+        window.setScene(studentListViewScene);
+
+        //show
+        window.show();
+    }
+}
